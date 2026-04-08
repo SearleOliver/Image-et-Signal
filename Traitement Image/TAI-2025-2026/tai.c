610497faa2c49775fc23a29c7d743406d1236185
NOTuglyduckling <osearle65@gmail.com>
2026-04-08 14:08:14 +0200

    histogram function

---
+++ Traitement Image/TAI-2025-2026/tai.c
@@ -0,0 +1,281 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <float.h>
+#include "limace.h"
+#include "tai.h"
+
+#define DEBOGAGE
+
+#ifdef DEBOGAGE
+#define DEBUG fprintf(stderr,"Fichier %s, ligne %d\n",__FILE__,__LINE__);
+#else
+#define DEBUG
+#endif
+
+#define AFAIRE(ValeurRetour) \
+  fprintf(stderr,"--> Fichier %s, ligne %d, corps de la fonction %s à écrire.\n",\
+          __FILE__,__LINE__,__func__);\
+  return ValeurRetour;  
+
+
+/*
+ * Conversion d'une image couleur en une image de niveaux de gris
+ * Entrée : image initiale en couleur
+ * Sortie : image de niveaux de gris résultat
+ */
+Image RGB2Gray(Image Im)
+{
+  int NbRow = ImNbRow(Im);
+  int NbCol = ImNbCol(Im);
+  Image Gray = ImAlloc(GrayLevel,NbRow,NbCol);
+  unsigned char** gray = ImGetI(Gray);
+  unsigned char** R = ImGetR(Im);
+  unsigned char** G = ImGetG(Im);
+  unsigned char** B = ImGetB(Im);
+  for (int i =0;i<NbRow;i++){
+    for (int j=0; j<NbCol;j++){
+     gray[i][j]= round((0.299*R[i][j])+(0.587*G[i][j])+(0.114*B[i][j]));
+    }
+  }
+  return Gray;
+}
+
+
+/*
+ * Binarisation d'une image de niveaux de gris par seuillage global
+ * Entrées : image de niveaux de gris initiale
+             seuil (niveau de gris)
+ * Sortie : image binaire
+ */
+Image Binarization(Image Im, unsigned char Threshold)
+{
+  int NbRow = ImNbRow(Im);
+  int NbCol = ImNbCol(Im);
+  unsigned char** gray = ImGetI(Im);
+  Image Bin = ImAlloc(BitMap,NbRow,NbCol);
+  unsigned char** bin = ImGetI(Bin);
+  for (int i =0;i<NbRow;i++){
+    for (int j=0; j<NbCol;j++){
+      if (gray[i][j]<Threshold)
+        bin[i][j]=0;
+      else 
+        bin[i][j]=1;
+    }
+  }
+  return Bin;
+}
+
+
+/*
+ * Inversion d'une image
+ * Entrée : image initiale (binaire, niveaux de gris ou couleur)
+ * Sortie : image résultat
+ */
+Image Inversion(Image Im)
+{
+  int NbRow = ImNbRow(Im);
+  int NbCol = ImNbCol(Im);
+  ImageType type = ImType(Im);
+  if (type==Color){
+    unsigned char** R = ImGetR(Im);
+    unsigned char** G = ImGetG(Im);
+    unsigned char** B = ImGetB(Im);
+    for (int i =0;i<NbRow;i++){
+      for (int j=0; j<NbCol;j++){
+        R[i][j]=255-R[i][j];
+        G[i][j]=255-G[i][j];
+        B[i][j]=255-B[i][j];
+      }
+    }
+  } else {
+    unsigned char** I = ImGetI(Im);
+    for (int i =0;i<NbRow;i++){
+      for (int j=0; j<NbCol;j++){
+        if (type==GrayLevel){
+          I[i][j]=255-I[i][j];
+        } else {
+          I[i][j]=1-I[i][j];
+        }
+      }
+    }
+  }
+  return Im;
+}
+
+
+/*
+ * Calcul de l'histogramme d'une image de niveaux de gris
+ * Entrée : image initiale (niveaux de gris)
+ * Sortie : histogramme (matrice de int 1 x 256)
+ */
+Matrix Histogram(Image Im)
+{
+  Matrix mat =  MatAlloc(Int,1,256);
+  int NbRow = ImNbRow(Im);
+  int NbCol = ImNbCol(Im);
+  int ** M=MatGetInt(mat);
+  for (int i=0; i < 256; i++){M[0][i]=0;}
+  unsigned char** I = ImGetI(Im);
+  for (int i =0; i<NbRow;i++){
+    for (int j = 0; j<NbCol;j++){
+      M[0][I[i][j]]+=1;
+    }
+  }
+  return mat;
+}
+
+
+/*
+ * Représentation d'un histogramme sous forme d'une image
+ * Entrées : histogramme (matrice de int 1 x 256) et nombre de lignes de
+ * l'image résultat (une échelle des niveaux de gris de 25 lignes est ajoutée
+ * sous l'histogramme)
+ * Sortie : image de niveaux de gris résultat
+ */
+Image Hist2Im(Matrix Hist, int NbLig)
+{
+	unsigned char **I;
+	int *h,i,j,Max=0,NbCol=256,NbLig2=NbLig+25;
+	Image Res;
+
+	if (MatType(Hist)!=Int) return NULL;
+  NbLig2=NbLig+25;
+	Res=ImCAlloc(GrayLevel,NbLig2,NbCol);
+  if (Res==NULL) return NULL;
+	h=*MatGetInt(Hist);
+	for (j=0;j<NbCol;j++)
+		if (h[j]>Max) Max=h[j];
+	I=ImGetI(Res);
+	for (j=0;j<256;j++)
+		for (i=NbLig-1;i>=(NbLig-NbLig*h[j]/Max);i--)
+		    I[i][j]=255;
+  for (j=0;j<256;j++)
+    I[NbLig][j]=0;
+  for (i=NbLig+1;i<NbLig2;i++)
+    for (j=0;j<256;j++)
+      I[i][j]=j;
+	return Res;
+}
+
+
+/*
+ * Calcul du seuil d'Otsu
+ * Entrée : histogramme (matrice de int 1 x 256)
+ * Sortie : seuil (niveau de gris) obtenu par la méthode d'Otsu
+ */
+unsigned char Otsu(Matrix Hist)
+{
+  
+}
+
+
+/*
+ * Calcul de l'histogramme cumulé à partir de l'histogramme
+ * Entrée : histogramme (matrice de int 1 x 256)
+ * Sortie : histogramme cumulé (matrice de int 1 x 256)
+ */
+Matrix Hist2CumHist(Matrix Hist)
+{
+  AFAIRE(NULL);
+}
+
+
+/*
+ * Application d'une transformation ponctuelle à une image de niveaux de gris
+ * Entreés : image initiale (niveaux de gris) et
+ * transformation ponctuelle (matrice de int 1 x 256)
+ * Sortie : image de niveaux de gris transformée
+ */
+Image AppLUT(Image Im, Matrix LUT)
+{
+  AFAIRE(NULL);
+}
+
+
+/*
+ * Spécification d'histogramme
+ * Entrées : histogramme cumulé de l'image et histogramme cumulé desiré
+ * (on suppose que le dernier élément des deux histogrammes cumulés sont
+ * les mêmes, c'est-à-dire qu'ils décrivent des images contenant le même nombre
+ * de pixels)
+ * Sortie : transformation ponctuelle (matrice 1 x 256)
+ */
+Matrix HistSpecif(Matrix CumHist, Matrix DesCumHist)
+{
+  AFAIRE(NULL);
+}
+
+
+/*
+ * Vérification de la validité d'une matrice représentant un élément
+ * structurant binaire (pour l'érosion, la dilatation, etc.)
+ * Entrée : matrice représentant un élément structurant
+ * Sortie : 0 si la matrice est valide,
+            SE_NOT_ODD si son nombre de lignes ou de colonnes n'est pas impair
+            SE_NOT_INT si elle ne contient pas que des entiers
+            SE_NOT_BIN si elle ne contient pas que des 0 et des 1
+*/
+int NotValidBinSE(Matrix StructuringElement)
+{
+  int **ES,NbLig,NbCol,i,j;
+
+  if (MatType(StructuringElement)!=Int)
+    return SE_NOT_INT;
+  NbLig=MatNbRow(StructuringElement);
+	if ((NbLig%2)!=1)
+	  return SE_NOT_ODD;
+  NbCol=MatNbCol(StructuringElement);
+	if ((NbCol%2)!=1)
+	  return SE_NOT_ODD;
+  ES=MatGetInt(StructuringElement);
+  for (i=0;i<NbLig;i++)
+    for (j=0;j<NbCol;j++)
+      if (ES[i][j]!=0 && ES[i][j]!=1)
+        return SE_NOT_BIN;
+  return 0;
+}
+
+
+/*
+ * Vérification de la validité d'une matrice représentant un élément
+ * structurant ternaire (pour la transformation "tout ou rien")
+ * Entrée : matrice représentant un élément structurant
+ * Sortie : 0 si la matrice est valide,
+            SE_NOT_ODD si son nombre de lignes ou de colonnes n'est pas impair
+            SE_NOT_INT si elle ne contient pas que des entiers
+            SE_NOT_TERN si elle ne contient pas que des 0, des 1 et des 2
+*/
+int NotValidTernSE(Matrix StructuringElement)
+{
+  int **ES,NbLig,NbCol,i,j;
+
+  if (MatType(StructuringElement)!=Int)
+    return SE_NOT_INT;
+  NbLig=MatNbRow(StructuringElement);
+	if ((NbLig%2)!=1)
+	  return SE_NOT_ODD;
+  NbCol=MatNbCol(StructuringElement);
+	if ((NbCol%2)!=1)
+	  return SE_NOT_ODD;
+  ES=MatGetInt(StructuringElement);
+  for (i=0;i<NbLig;i++)
+    for (j=0;j<NbCol;j++)
+      if (ES[i][j]!=0 && ES[i][j]!=1 && ES[i][j]!=2)
+        return SE_NOT_TERN;
+  return 0;
+}
+
+
+/*
+ * Amincissement d'une image binaire
+ * Entreés : image binaire initiale et élément structurant (matrice de int
+ * contenant uniquement des 0, des 1 et des 2 signifiant "peu importe")
+ * Sortie : image binaire transformée
+ */
+Image Thinning(Image Im, Matrix StructuringElement)
+{
+  AFAIRE(NULL);
+}
+
